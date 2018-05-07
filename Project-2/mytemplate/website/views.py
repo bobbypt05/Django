3d4fa03adb2cfc4865669d9426347a48bc14abf9
BOBBY <bobbypt05@gmail.com>
2018-05-07 16:18:46 +0530

    templating added

---
+++ Project-2/mytemplate/website/views.py
@@ -0,0 +1,12 @@
+from  django.views.generic import TemplateView
+
+# Create your views here.
+
+class HomePageView(TemplateView):
+	template_name = 'index.html'
+
+class AboutPageView(TemplateView):
+	template_name = 'about.html'
+
+class ContactPageView(TemplateView):
+	template_name = 'contact.html'
