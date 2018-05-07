69aa6790ab31e8783bc27a2cf14c6fe1c5488238
BOBBY <bobbypt05@gmail.com>
2018-05-07 13:47:13 +0530

    Hello World in Hindi and marathi added

---
+++ Project-1/namaste/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def function(request):
+	return HttpResponse('Namaste means hello in hindi');
